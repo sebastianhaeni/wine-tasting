0c6769b9b52f5b34b9caf1d2c65768e88b9abcbd
Sebastian Häni <haeni.sebastian@gmail.com>
2016-10-26 21:02:14 +0200

    added manifest and service worker, forcing ssl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,3 +2,13 @@ require('./admin');
 require('./results');
 require('./voting');
 require('./welcome');
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js').then(function (registration) {
+		// Registration was successful
+		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	}).catch(function (err) {
+		// registration failed :(
+		console.log('ServiceWorker registration failed: ', err);
+	});
+}
--- gulpfile.js
+++ gulpfile.js
@@ -10,74 +10,76 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var del = require('del');
 
-gulp.task('default', function() {
-    runSequence('clean', ['sass', 'nunjucks', 'js', 'images']);
+gulp.task('default', function () {
+	runSequence('clean', ['sass', 'nunjucks', 'js', 'images']);
 });
 
-gulp.task('clean', function() {
-    return del(['www/**/*']);
+gulp.task('clean', function () {
+	return del(['www/**/*']);
 });
 
-gulp.task('sass', function() {
-  gulp.src('app/styles/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./www/css'));
+gulp.task('sass', function () {
+	gulp.src('app/styles/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./www/css'));
 });
 
-gulp.task('nunjucks', function() {
-    nunjucksRender.nunjucks.configure(['app/templates/'], {watch: false});
-    return gulp.src('app/pages/**/*.html')
-       .pipe(nunjucksRender())
-       .on('error', function(error) {
-           gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
-           this.emit('end');
-       })
-       .pipe(gulp.dest('./www'));
+gulp.task('nunjucks', function () {
+	nunjucksRender.nunjucks.configure(['app/templates/'], {watch: false});
+	return gulp.src('app/pages/**/*.html')
+		.pipe(nunjucksRender())
+		.on('error', function (error) {
+			gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./www'));
 });
 
-gulp.task('js', function() {
-    var bundler = browserify('app/scripts/app.js').transform(babel, {presets: ['es2015']});
+gulp.task('js', function () {
+	var bundler = browserify('app/scripts/app.js').transform(babel, {presets: ['es2015']});
 
-    bundler.bundle()
-        .on('error', function(err) { 
-        	console.error(err); 
-        	this.emit('end'); 
-        })
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(gulp.dest('www/js'));
+	bundler.bundle()
+		.on('error', function (err) {
+			console.error(err);
+			this.emit('end');
+		})
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('www/js'));
 
-    return;
+	return gulp.src('app/scripts/sw.js')
+		.pipe(gulp.dest('./www'));
+
 });
 
-gulp.task('images', function() {
-    gulp.src('app/images/**/*.{png,jpg,gif}')
-        .pipe(gulp.dest('www/images'));
+gulp.task('images', function () {
+	gulp.src('app/images/**/*.{png,jpg,gif}')
+		.pipe(gulp.dest('www/images'));
 });
 
-gulp.task('server', ['default'], function() {
-    var server = gls.new('./build/server.js');
-    server.start();
+gulp.task('server', ['default'], function () {
+	var server = gls.new('./build/server.js');
+	server.start();
 
-    // Restart the server when file changes
-    gulp.watch(['app/styles/**/*.scss'], function(file){
-        runSequence('sass', function() {
-            server.notify.apply(server, [file]);
-        });
-    });
-    gulp.watch(['app/**/*.html'], function(file){
-        runSequence('nunjucks', function() {
-            server.notify.apply(server, [file]);
-        });
-    });
-    gulp.watch(['app/scripts/**/*.js'], function(file){
-        runSequence('js', function() {
-            server.notify.apply(server, [file]);
-        });
-    });
-    gulp.watch(['app/images/**/*.{png,jpg,gif}'], function(file){
-        runSequence('images', function() {
-            server.notify.apply(server, [file]);
-        });
-    });
+	// Restart the server when file changes
+	gulp.watch(['app/styles/**/*.scss'], function (file) {
+		runSequence('sass', function () {
+			server.notify.apply(server, [file]);
+		});
+	});
+	gulp.watch(['app/**/*.html'], function (file) {
+		runSequence('nunjucks', function () {
+			server.notify.apply(server, [file]);
+		});
+	});
+	gulp.watch(['app/scripts/**/*.js'], function (file) {
+		runSequence('js', function () {
+			server.notify.apply(server, [file]);
+		});
+	});
+	gulp.watch(['app/images/**/*.{png,jpg,gif}'], function (file) {
+		runSequence('images', function () {
+			server.notify.apply(server, [file]);
+		});
+	});
 });
